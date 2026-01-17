626eb2f8ea26996097bf24c6bffadbee63007978
IvanArriaza <ceradudelfin@gmail.com>
2026-01-17 20:48:05 +0100

    Create StartWeb.py

---
+++ Example Codes/StartWeb.py
@@ -0,0 +1,26 @@
+#This code was written with the purpose of understanding how this hid oriented librarys work. I use an Spanish keyboard layout, which you can easily get. 
+
+import usb_hid
+import time
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+from keyboard_layout_win_es import KeyboardLayout
+
+
+kb=Keyboard(usb_hid.devices)
+ly=KeyboardLayout(kb)
+
+#Open CMD
+kb.send(Keycode.WINDOWS)
+kb.send(Keycode.C,Keycode.M,Keycode.D)
+time.sleep(0.5)
+kb.press(Keycode.ENTER)
+kb.release_all()
+
+#CMD command
+time.sleep(0.5)
+ly.write("start https://netflix.com")
+kb.press(Keycode.ENTER)
+kb.release_all()
+
+#As You can see, It simply opens the netflix main webpage upon plugin the device in.
